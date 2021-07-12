ed29ee0c8176e33b4f0ae3c26cef0a3fed5ad0d6
ciclarimpo <74079434+ciclarimpo@users.noreply.github.com>
2021-07-12 19:08:55 +0200

    Update professionista_registrato.php

--- professionista_registrato.php
+++ professionista_registrato.php
@@ -26,6 +26,8 @@ class Prof_Reg {
         $this->email = $_email;
     }
 
+    
+    
     //METODI CLASSICI & BANALI//
     public function setName(string $nome) {$this->name = $nome;}
     public function getName() : string {return $this->name;}
@@ -45,6 +47,26 @@ class Prof_Reg {
     public function setEmail(string $mail) {$this->email = $mail;}
     public function getEmail() : string {return $email;}
 
+    
+    //METODI SPECIFICI//
+    //public function login()
+    //public function logout()
+    public function accedi_a_doc(string $doc_name) : Documento {}
+    //public function edit_doc() {}
+    //public function delete_doc() {}
+    //public function add_doc_comment() {}
+    //public function open_forum() {}
+    public function publish_q(string $question) : Domanda {}
+    public function answer(string $question, string $answer) : risposta {}
+    public function serch_q(string $keywords) : Domanda {}
+    public function add_doc_fav(string $doc_name, string $username) {}
+    public function delete_doc_fav(string $doc_name, string $username) {}
+    //public function view_fav() {}
+    public function select_cat(string $cat_name, bool $reg) : Categoria {}
+    public function save_doc(string $doc_name, string $cat_name) {}
+    public function crea_categoria(string $cat_name) : Categoria {}
+    public function delete_doc(string $doc_name) {}
+    public function select_cat(string $cat_name, bool $reg) : Categoria
 
 
 }
